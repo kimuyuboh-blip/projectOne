703b54e6b04119b23556fe662dba729fa3634a26
Kimuyu Carlos <kimuyuboh@gmail.com>
2025-11-16 19:05:26 +0300

    added vercel seep insights

--- brandWork/src/App.jsx
+++ brandWork/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "/src/Home.jsx";
 import Dashboard from "./components/sections/Dashboard.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
