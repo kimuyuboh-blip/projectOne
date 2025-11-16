a111c481688b77d78031ad4c270cb445e139de58
Kimuyu Carlos <kimuyuboh@gmail.com>
2025-11-16 19:28:57 +0300

    removed unnecessary imports

--- brandWork/src/App.jsx
+++ brandWork/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "/src/Home.jsx";
 import Dashboard from "./components/sections/Dashboard.jsx";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
